0e951fe44ea61b3b55c3d77bf4c4e25eb09a12c4
Chris <chris.g14@iiits.in>
2018-09-22 23:59:22 +0530

    Spatial Cureing data and parser.

---
+++ parse_data.py
@@ -0,0 +1,64 @@
+from lxml import etree
+import matplotlib.pyplot as plt
+import os
+import pdb
+
+
+def parse_file(filepath):
+    """Parse the HTML file to get data."""
+    html = open(filepath).read().replace("\n", "")
+    data_table = etree.HTML(html).findall("body/table")[1]
+
+    rows = data_table.getchildren()
+
+    raw_data = {}
+    columns = []
+
+    for headers in rows[0].getchildren():
+        label = headers.getchildren()[0].text
+        raw_data[label] = list()
+        columns.append(label)
+
+    for r in rows[1::]:
+        for i, entry in enumerate(r.getchildren()):
+            value = entry.getchildren()[0].text
+            raw_data[columns[i]].append(value)
+
+    trial, time = raw_data[columns[1]], raw_data[columns[2]]
+    data = {}
+    for l, t in zip(trial, time):
+        if l not in data:
+            data[l] = [float(t)]
+        else:
+            data[l].append(float(t))
+
+    return data
+
+
+def get_files(data_path):
+    """Get all the file in the data path."""
+    file_list = os.listdir(data_path)
+    file_list = [x for x in file_list if x[-5:] == ".html"]
+    file_list = [os.path.join(data_path, x) for x in file_list]
+    return file_list
+
+
+def find_means(data):
+    means = {}
+    for key in data.keys():
+        means[key] = sum(data[key]) / len(data[key])
+    return means
+
+
+if __name__ == "__main__":
+    data_path = "data/"
+    files = get_files(data_path)
+    data = [parse_file(x) for x in files]
+    means = [find_means(x) for x in data]
+
+    for key in means[0].keys():
+        data = [x[key] for x in means]
+        plt.plot(data, label=key)
+    plt.legend()
+    plt.show()
+    pdb.set_trace()
